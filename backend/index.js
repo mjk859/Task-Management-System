f8474a8ed585873b54850e5c5903c381fe65ea83
Jaseel <muhammedjaseelk10@gmail.com>
2023-09-01 22:33:09 +0530

    use state hooks

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,38 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
+const port = 3001
+
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb+srv://admin-jaseel:VVzw3hMS0UMMsJRU@cluster0.o1fsbhk.mongodb.net/taskManagerDB');
+
+const userSchema = new mongoose.Schema({
+    username: String,
+    email: String,
+    password: String
+});
+const User = mongoose.model('User', userSchema);
+
+const taskSchema = new mongoose.Schema({
+    title: String,
+    description: String,
+    dueDate: String,
+    status: String,
+    assignedUser: String
+});
+const Task = mongoose.model('Task', taskSchema);
 
 app.get('/', (req, res) => {
     res.send('Welcome');
 });
 
-app.listen('1337', (req, res) => {
-    console.log("listening on port 1337" );
+app.get('/register', (req, res) => {
+    res.send('register page');
+});
+
+app.listen(port, (req, res) => {
+    console.log("listening on port", port);
 });
